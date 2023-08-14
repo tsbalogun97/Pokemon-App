ab3a9dacf265160a2976d305589b88c83dd58373
Temitope Balogun <tsbalogun97@yahoo.com>
2023-08-14 10:09:29 -0500

    index.jsx view rendered at pokemon route

---
+++ server.js
@@ -0,0 +1,46 @@
+const pokemon = require("./models/pokemon");
+// importing pokemon.js from models
+
+const express = require("express");
+
+
+const app = express();
+
+
+
+const PORT = 3000;
+
+
+
+// ------ middleware
+
+app.set('view engine', 'jsx');
+// setting view engin jsx
+
+
+app.engine('jsx', require('express-react-views').createEngine());
+// creating our engine
+
+app.use(express.json());
+// allow us to read client's request.body
+
+app.use(express.urlencoded({ extended: false }));
+// accepting data in a specific format 
+
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to Pokemon App!</h1>");
+});
+
+app.get("/pokemon", (req, res)=>{
+  res.render("Index")
+})
+
+
+
+
+
+app.listen(PORT, function() {
+  console.log(`Listening on ${PORT}`);
+});
---
+++ views/Index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Index() {
+  const myStyle = {
+    color: "#ffffff",
+    backgroundColor: "#000000",
+  };
+  return <h1 style={myStyle}>See All The PokeMon</h1>;
+}
+
+export default Index;
